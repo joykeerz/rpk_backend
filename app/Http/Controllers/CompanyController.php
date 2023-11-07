26c6d7adf47d8f823448f52b2644e49fdb255216
joykeerz <wisnu.mukti10@gmail.com>
2023-11-07 20:47:29 +0700

    alamat update

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -108,15 +108,34 @@ class CompanyController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validated = $request->validate([
+            'tb_kode_company' => 'required|unique:companies,kode_company',
+        ],[
+            'tb_kode_company.required' => 'Kode Company harus diisi',
+            'tb_kode_company.unique' => 'Kode Company sudah terdaftar',
+        ]);
+
         $company = Company::findOrFail($id);
-        $company->alamat_id = $request->tb_alamat_id;
         $company->user_id = $request->tb_user_id;
         $company->kode_company = $request->tb_kode_company;
         $company->nama_company = $request->tb_nama_company;
         $company->partner_company = $request->tb_partner_company;
         $company->tagline_company = $request->tb_tagline_company;
         $company->save();
+
+        $alamat = Alamat::findOrFail($company->alamat_id);
+        $alamat->jalan = $request->tb_jalan;
+        $alamat->jalan_ext = $request->tb_jalan_ext;
+        $alamat->blok = $request->tb_blok;
+        $alamat->rt = $request->tb_rt;
+        $alamat->rw = $request->tb_rw;
+        $alamat->provinsi = $request->tb_prov;
+        $alamat->kota_kabupaten = $request->tb_kota;
+        $alamat->kecamatan = $request->tb_kecamatan;
+        $alamat->kelurahan = $request->tb_kelurahan;
+        $alamat->negara = 'indonesia';
+        $alamat->kode_pos = $request->tb_kodepos;
+        $alamat->save();
 
         $res =  response()->json([
             'data' => $company,
