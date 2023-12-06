5f3e630dd1d2b6ac84575ce66666bbd2b8ff882d
joykeerz <wisnu.mukti10@gmail.com>
2023-12-06 09:44:11 +0700

    fixed migrations

--- database/migrations/2023_10_24_032824_create_alamats_table.php
+++ database/migrations/2023_10_24_032824_create_alamats_table.php
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('alamats');
+        Schema::dropIfExists('alamat');
     }
 };
--- database/migrations/2023_11_27_071458_create_satuan_units_table.php
+++ database/migrations/2023_11_27_071458_create_satuan_units_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('satuan_units');
+        Schema::dropIfExists('satuan_unit');
     }
 };
